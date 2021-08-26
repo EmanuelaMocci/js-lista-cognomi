51da7b27bdee690d48ee391b13885e207bc7eb90
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-26 14:49:31 +0200

    Aggiunta promt, array e ordine alfabetico

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// 1. chiedi all’utente il cognome
+var tuoCognome = prompt("Inserisci il tuo cognome");
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+cognomi.push(tuoCognome); 
+console.log(cognomi);
+
+// 3. stampa la lista ordinata alfabeticamente
+cognomi.sort();
+console.log(cognomi);
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
