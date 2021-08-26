1e3f1a6e78120068ff33b6626baf3088b1a0d90f
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-26 15:06:41 +0200

    Aggiunta pisizione "umana" del cognome inserito

--- js/script.js
+++ js/script.js
@@ -11,3 +11,5 @@ cognomi.sort();
 console.log(cognomi);
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+var posizione = cognomi.indexOf(tuoCognome) +1;
+console.log(posizione);
